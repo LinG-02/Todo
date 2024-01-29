f1e446bff0096249adee30a89fc6f84ce75c9123
LinG-02 <74174742+LinG-02@users.noreply.github.com>
2024-01-29 14:25:55 +1100

    version1.2-database

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'user1', 'Learn JPA', CURRENT_DATE(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'user1', 'Learn Database', CURRENT_DATE(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'user1', 'Learn Node.js', CURRENT_DATE(), false);
